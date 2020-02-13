9e8d3995eeaddaa3d6bbe1ed0841fb3d02bac05b
Kyle Talmage <ktalmage85@gmail.com>
2020-02-13 18:32:51 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i+=1
+  end
 end
+collection = [1,2,3]
+puts my_all?(collection) {|i| i*i}
